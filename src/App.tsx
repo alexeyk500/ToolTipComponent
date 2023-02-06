ea078c97eb012d7f368d5d53dbbb0b48e27eb337
alexeyk500 <alexeyk500@yandex.ru>
2023-02-06 11:23:44 +0300

    PureCSS - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import classes from './App.module.css';
-import ToolTipComponent from './Components/ToolTipComponent/ToolTipComponent';
 import ButtonComponent from './Components/ButtonComponent/ButtonComponent';
 
 function App() {
   return (
     <div className={classes.container}>
-      <ToolTipComponent text={'Я подсказка'} customClass={classes.toolTipCustom}>
-        <ButtonComponent />
-      </ToolTipComponent>
       <div className={classes.buttonContainer}>
         <span data-hint="Я подсказка">
           <ButtonComponent />
